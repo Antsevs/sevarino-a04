fa9c1b6c8d89cadd80ada62b4f724e3a980b6476
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-17 17:14:29 -0400

    Solution 41 initial commit with psuedocode

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution 41
+ *  Copyright 2021 Anthony Sevarino
+ */
+
+
+/* have input and output file ready in baseline
+ * create function to sort names alphabetically until no more entries in input
+ * add the names from the input file
+ * write these names to output file listing number of names
+ * close to avoid mem leak
+ * scan input file
+ * insert names into array and sort
+ */
+
+
+package baseline;
+
+public class Solution41 {
+    public static void main(String[] args) {
+
+    }
+}
