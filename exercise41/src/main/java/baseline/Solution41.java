830b99cae44e4b03dcc76b129b37b893e0ef0db1
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-17 18:50:32 -0400

    Second commit with full code and test cases

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -16,8 +16,46 @@
 
 package baseline;
 
-public class Solution41 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Solution41 {
+
+    public static void inp_File(Scanner scnr, ArrayList<String> names){
+        //continue reading names until there are none remaining
+        while(scnr.hasNextLine()){
+            names.add(scnr.nextLine());
+        }
+    }
+
+
+    public static void out_File(ArrayList<String> names) throws IOException {
+        FileWriter out = new FileWriter("exercise41_output.txt");
+        out.write("Total of " + names.size() + " names\n");
+        out.write("----------\n");
+
+        for (String i : names) {
+            out.write(i + "\n");
+        }
+
+        out.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scnr = new Scanner(new File("exercise41_input.txt"));
+
+        ArrayList<String> names = new ArrayList<>();
+
+        inp_File(scnr, names);
+
+
+        names.sort(String.CASE_INSENSITIVE_ORDER);
+
+        scnr.close();
+
+        out_File(names);
     }
 }
