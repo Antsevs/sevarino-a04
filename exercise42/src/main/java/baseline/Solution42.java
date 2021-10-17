1a14b6613d32bea1c2eb0f0e3ebb5c6dabfd1b37
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-17 19:55:13 -0400

    second commit with full code and test cases

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -11,6 +11,29 @@
 
 package baseline;
 
-public class Solution42 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Solution42 {
+    public void entry(String output){
+        try {
+            File out = new File(output);
+            Scanner scnr = new Scanner(out);
+            while(scnr.hasNext()){
+                String strng = scnr.nextLine();
+                String[] x = strng.split(",");
+                System.out.printf("%-10s%-10s%-10s\n", x[0], x[1], x[2]);
+            }
+        } catch (FileNotFoundException ex){
+            System.out.println("File Not Found");
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution42 data = new Solution42();
+        System.out.printf("%-10s%-10s%-10s\n", "Last", "First", "Salary");
+        System.out.println("----------------------------");
+        data.entry("exercise42_input.txt");
+    }
 }
