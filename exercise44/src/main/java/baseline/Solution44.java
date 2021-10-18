c851598c375a8ccdea3b7622e1752beb2578efdc
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-17 22:51:36 -0400

    initial commit with psuedocode

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution 44
+ *  Copyright 2021 Anthony Sevarino
+ */
+
+
+/*
+ * store data from file in object
+ * set products in array and take user input
+ * get user information
+ * compare input and JSON object
+ * if matching, give pertinent information
+ */
+
+package baseline;
+
+public class Solution44 {
+}
