eb5c9032a30ac1b3ee5df352183e504f02f6ca8f
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-17 20:12:40 -0400

    initial commit with psuedocode

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution 43
+ *  Copyright 2021 Anthony Sevarino
+ */
+
+
+/*
+ * Ask user for name of site, author, and if they want folder for java and css files
+ * create index.html with above information
+ * output to program directory
+ */
+
+package baseline;
+
+public class Solution43 {
+
+}
